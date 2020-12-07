a16f00dbd301eebba3a392b426446001b34ae25a
Przemyslaw Pajak <office@fearlessspider.com>
2020-12-07 19:32:36 +0100

    feat(Vagrant): add Vagrantfile

---
+++ Vagrantfile
@@ -0,0 +1,17 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+Vagrant.configure('2') do |config|
+  config.vm.box      = 'ubuntu/bionic64'
+  config.vm.hostname = 'ai-with-python'
+
+  config.vm.network :forwarded_port, guest: 3000, host: 3000
+  config.vm.network :forwarded_port, guest: 8888, host: 8888
+
+  config.vm.provider "virtualbox" do |v|
+    v.memory = 512
+    v.cpus = 2
+  end
+
+  config.vm.provision :shell, path: 'bootstrap.sh', keep_color: true
+  config.vm.synced_folder "notebooks/", "/home/vagrant/notebooks"
+end
---
+++ bootstrap.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+##################################
+# AI with Python Vagrant Machine #
+#     by FEARLESS SPIDER         #
+##################################
+function install {
+    echo installing "$1"
+    shift
+    apt-get -y install "$@" >/dev/null 2>&1
+}
+
+function pip_install {
+    echo installing "$1"
+    shift
+    pip3 install "$@" >/dev/null 2>&1
+}
+
+echo "updating package information"
+apt-get -y update >/dev/null 2>&1
+
+# Theano
+install 'pip3' python3-pip build-essential libssl-dev libffi-dev
+install 'theano dependencies' python3-numpy python3-scipy python3-dev python3-nose g++ git libatlas3gf-base libatlas-dev
+pip_install 'theano' theano
+
+# Keras
+pip_install 'keras' keras
+
+# Tensorflow
+pip_install 'tensorflow' tensorflow
+
+# Miscellaneous
+pip_install 'required Python libraries' pyyaml cython
+install 'hdf5' libhdf5-7 libhdf5-dev
+pip_install 'h5py' h5py
+pip_install 'ipython' ipython
+pip_install 'jupyter' jupyter
+pip_install 'scikit-learn' scikit-learn
+install 'matplotlib' matplotlib
+
+echo 'All set!'
